ab0b5211eaba49fdce6a7a2b17faefd0170575fb
you@example.com <you@example.com>
2024-11-15 03:09:49 +0300

    request pos

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,6 @@
 function updateBackgroundPosition() {
     const imageBlock = document.querySelector('header');
     const width = window.innerWidth;
-
     if (width <= 768) {
         imageBlock.style.backgroundPosition = 'right';
     } else {
@@ -14,3 +13,31 @@ updateBackgroundPosition();
 
 // Обновляем позицию фона при изменении размера окна
 window.addEventListener('resize', updateBackgroundPosition);
+
+const textarea = document.getElementsByClassName('contact_form_text')[0];
+const placeholder = document.getElementsByClassName('placeholder')[0];
+console.log(textarea)
+
+
+// Функция для обновления видимости плейсхолдера
+function updatePlaceholder() {
+    if (textarea.value === '') {
+        placeholder.style.display = 'block';
+    } else {
+        placeholder.style.display = 'none';
+    }
+}
+
+// Скрываем плейсхолдер при фокусировке
+textarea.addEventListener('focus', () => {
+    placeholder.style.display = 'none';
+});
+
+// Проверяем текст при потере фокуса
+textarea.addEventListener('blur', updatePlaceholder);
+
+// Проверяем текст при вводе
+textarea.addEventListener('input', updatePlaceholder);
+
+// Инициализация видимости плейсхолдера
+updatePlaceholder();
